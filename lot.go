32b9055be885cc756aea02aae7cc9c8ff2ed1ed5
k-tomoyasu <blhicr1303@gmail.com>
2021-01-24 22:22:08 +0900

    return error when no member

--- lot.go
+++ lot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"time"
@@ -18,13 +19,28 @@ type Lot struct {
 // DrawLots decide member randomly and send message to slack.
 func (l *Lot) DrawLots(channelID string, members []Member, userGroupID string) error {
 	if len(members) == 0 {
-		return nil
+		return errors.New("no member found")
 	}
 	rng := rand.New(mt19937.New())
 	rng.Seed(time.Now().UnixNano())
-	winner := members[rng.Intn(len(members))]
+	winner := draw(members, rng.Intn)
+	messages := buildLotMessage(winner, userGroupID, l.messageTemplate)
+	if _, _, err := l.client.PostMessage(channelID, messages...); err != nil {
+		return fmt.Errorf("failed to post message: %s", err)
+	}
+	return nil
+}
+
+func draw(members []Member, rngfn func(int) int) Member {
+	return members[rngfn(len(members))]
+}
+
+func buildLotMessage(winner Member, userGroupID string, template MessageTemplate) []slack.MsgOption {
+	var messages []slack.MsgOption
+	messages = append(messages, slack.MsgOptionText(fmt.Sprintf("<@%s>", winner.ID), false))
+
 	attachment := slack.Attachment{
-		Text:       fmt.Sprintf(l.messageTemplate.Choose, winner.Name),
+		Text:       fmt.Sprintf(template.Choose, winner.Name),
 		Color:      "#42f46e",
 		CallbackID: "taskuji",
 		Actions: []slack.AttachmentAction{
@@ -43,13 +59,9 @@ func (l *Lot) DrawLots(channelID string, members []Member, userGroupID string) e
 				Value: userGroupID,
 			},
 		},
-		Title:  l.messageTemplate.LotTitle,
+		Title:  template.LotTitle,
 		Footer: "Push the Button",
 	}
-	textMsg := slack.MsgOptionText(fmt.Sprintf("<@%s>", winner.ID), false)
-	attachmentMsg := slack.MsgOptionAttachments(attachment)
-	if _, _, err := l.client.PostMessage(channelID, textMsg, attachmentMsg); err != nil {
-		return fmt.Errorf("failed to post message: %s", err)
-	}
-	return nil
+	messages = append(messages, slack.MsgOptionAttachments(attachment))
+	return messages
 }
